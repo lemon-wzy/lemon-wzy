232792f0098748d0e4fa16ed1acbdaf6643efb05
lemon-wzy <wzyhlj999455@163.com>
2021-08-09 10:33:26 +0800

    新增二维码生成包

---
+++ src/main/java/shiro/demo/utils/QRCodeGeneratorUtil.java
@@ -0,0 +1,9 @@
+package shiro.demo.utils;
+
+/**
+ * @author lichi
+ */
+public class QRCodeGeneratorUtil {
+
+//    private static final String QR_CODE_IMAGE_PATH  = 'D:\gitHubProject\\lemon-wzy\\lemon-wzy\\src\\main\\resources\\lib\\MyQRCode.png'
+}
