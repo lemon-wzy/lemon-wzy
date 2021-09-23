3eaaa46adfd852a6172433fb8f3f8b4683705d6b
[王志颖] <wzyhlj999455@163.com>
2021-09-23 16:59:27 +0800

    配置文件脱敏

--- src/test/java/shiro/demo/DemoApplicationTests.java
+++ src/test/java/shiro/demo/DemoApplicationTests.java
@@ -2,6 +2,8 @@ package shiro.demo;
 
 
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import org.jasypt.encryption.StringEncryptor;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mybatis.spring.annotation.MapperScan;
@@ -26,19 +28,38 @@ public class DemoApplicationTests {
 	@Autowired
 	private IGlobalCache iGlobalCache;
 
+	@Autowired
+	private StringEncryptor stringEncryptor;
+
+	@Test
+	public void encrypt() {
+//		String url = encryptor.encrypt("jdbc\\:mysql\\://127.0.0.1\\:3306/test?useUnicode\\=true&characterEncoding\\=UTF-8&zeroDateTimeBehavior\\=convertToNull&useSSL\\=false&allowMultiQueries\\=true&serverTimezone=Asia/Shanghai");
+		String url = stringEncryptor.encrypt("jdbc:mysql://110.42.250.78:3306/test?useUnicode=true&characterEncoding=utf-8&useSSL=true&serverTimezone=UTC");
+		String name = stringEncryptor.encrypt("root");
+		String password = stringEncryptor.encrypt("root");
+		System.out.println("database url: " + url);
+		System.out.println("database name: " + name);
+		System.out.println("database password: " + password);
+		Assert.assertTrue(url.length() > 0);
+		Assert.assertTrue(name.length() > 0);
+		Assert.assertTrue(password.length() > 0);
+	}
+
+
+
 
 
 
 //	@Autowired
 //	private IOsAreaService iOsAreaService;
 
-	@Test
-	public void  test() {
-		iGlobalCache.set("key2","value2");
-		iGlobalCache.lSetAll("list", Arrays.asList("hello","redis"));
-		List<Object> list = iGlobalCache.lGet("list",0,-1);
-		System.out.println(iGlobalCache.get("key2"));
-	}
+//	@Test
+//	public void  test() {
+//		iGlobalCache.set("key2","value2");
+//		iGlobalCache.lSetAll("list", Arrays.asList("hello","redis"));
+//		List<Object> list = iGlobalCache.lGet("list",0,-1);
+//		System.out.println(iGlobalCache.get("key2"));
+//	}
 
 //	@Test
 //	public void testSQl() {
